e40ad36c49b0b1e4d2acf03d84d2fd81d60bec5c
Samoilenko S. <fb.alferova.s@gmail.com>
2025-02-05 00:14:05 +0200

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -37,7 +37,7 @@ const App = () => {
       <Route
         path="/register"
         element={
-          <RestrictedRoute redirectTo="/" component={<RegisterPage />} />
+          <RestrictedRoute redirectTo="/" component={<RegistrationPage />} />
         }
       />
       <Route
